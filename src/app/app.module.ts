391b211c7054a01a23eefca0b5dcc2879b375b14
Glen Roselle <glenroselle@gmail.com>
2019-05-28 20:04:47 -0400

    updated frontend with http module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule} from '@angular/common/http';
+
 
 import { AppComponent } from './app.component';
 import { ValueComponent } from './value/value.component';
@@ -9,7 +11,7 @@ import { ValueComponent } from './value/value.component';
 
   ],
   imports: [
-    BrowserModule
+    BrowserModule, HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/value/value.component.ts
+++ src/app/value/value.component.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';// needed for http injection
 
 @Component({
-  selector: 'app-value',
+  selector: 'valuetest',
   templateUrl: './value.component.html',
   styleUrls: ['./value.component.css']
 })
 export class ValueComponent implements OnInit {
   values: any;
-
+  // this contsructer is injecting the http services into the constructor so we can use it in our class
   constructor(private http: HttpClient) { }
 
   ngOnInit() { // A lifecycle method.
